ae4fea3339fb3c30b16e1417921ca4b22c5f1bd2
John <johnjerry8749@gmail.com>
2026-08-12 22:23:35 +0100

    refactor: Clean up Navbar component structure and styling

--- Client/src/components/common/Navbar.jsx
+++ Client/src/components/common/Navbar.jsx
@@ -8,34 +8,40 @@ const Navbar = () => {
         <p className="mb-0 ps-3">+1234567890</p>
         <p className="mb-0 ps-3">support@yourstore.com</p>
       </div>
-      <div className=" border border-dark bg-white row d-flex justify-content-center  align-items-center m-auto mt-0">
+      <div className="bg-white row d-flex justify-content-center  align-items-center m-auto mt-0">
         <div className="container">
           <div className="row align-items-center d-flex gap-3 pe-2 justify-content-center">
-            <div className="col-3 border border-danger py-3">
-              <div className="border border-danger">
+            <div className="col-3 py-3">
+              <div className="image-container ps-2 mt-2">
                 <img
                   src={Logo}
                   alt="logo"
-                  className="img-fluid"
+                  style={{ width: "200px", height: "auto" }}
                 />
               </div>
             </div>
-            <div className="col-4 fs-4 border d-flex gap-4 p-4 justify-content-start border-danger">
-             <Link to="/" className="text-decoration-none text-dark">Home</Link>
-             <Link to="/" className="text-decoration-none text-dark">Collection</Link>
-             <Link to="/" className="text-decoration-none text-dark">About</Link>
-             <Link to="/" className="text-decoration-none text-dark">Contact</Link>
-
+            <div className="col-4 fs-4  d-flex gap-4 p-4 justify-content-start ">
+              <Link to="/" className="text-decoration-none text-dark">
+                Home
+              </Link>
+              <Link to="/" className="text-decoration-none text-dark">
+                Collection
+              </Link>
+              <Link to="/" className="text-decoration-none text-dark">
+                About
+              </Link>
+              <Link to="/" className="text-decoration-none text-dark">
+                Contact
+              </Link>
             </div>
             <div className="col-3 border border-danger">
-              
-              
-              One of three columns</div>
+              One of three columns
+            </div>
           </div>
         </div>
       </div>
     </div>
-  ); 
+  );
 };
 
 export default Navbar;

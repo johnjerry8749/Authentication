9bdc708f427ede932e54dffcf95dbdfbbd64b41c
John <johnjerry8749@gmail.com>
2026-08-18 00:18:16 +0100

    save

--- Client/src/components/pages/About.jsx
+++ Client/src/components/pages/About.jsx
@@ -6,7 +6,13 @@ function About() {
   return (
     <div>
       <Navbar />
-      <div className="border border-danger p-2"></div>
+      <div className="border border-danger mb-4 p-2">
+        <div className="d-flex justify-content-center mt-4 align-items-center">
+        <h3 className="mt-3 text-center fw-bold text-muted">ABOUT <span className="text-dark">US</span></h3>
+        <div className="mt-3  " style={{width:"30px", height:"2px", backgroundColor:"black"}}></div>
+        </div>
+
+      </div>
         
     <Newsletter />    
     <Footer />
--- Client/src/components/pages/Contact.jsx
+++ Client/src/components/pages/Contact.jsx
@@ -7,9 +7,9 @@ const Contact = () => {
     <div>
       <Navbar />
       <div className="border border-danger mb-4 p-2">
-        <div>
-        <h3 className=" border border-danger mt-3 text-center fw-bold text-muted">ABOUT <span className="text-dark">US</span></h3>
-        <div style={{width:"50px", height:"1px", backgroundColor:"black"}}></div>
+        <div className="d-flex justify-content-center mt-4 align-items-center">
+        <h3 className="mt-3 text-center fw-bold text-muted">CONTACT <span className="text-dark">US</span></h3>
+        <div className="mt-3  " style={{width:"30px", height:"2px", backgroundColor:"black"}}></div>
         </div>
 
       </div>

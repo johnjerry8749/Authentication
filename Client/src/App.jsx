a01f27df09be4020fb7a88af470b0bc5cac8e040
John <johnjerry8749@gmail.com>
2026-08-17 20:14:33 +0100

    save up

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,19 +1,23 @@
-import { Route, Routes } from 'react-router-dom';
-import AllHomecomponents from './components/pages/AllHomecomponents.jsx';
-import ProductDetails from './components/pages/ProductDetails.jsx'
+import { Route, Routes } from "react-router-dom";
+import AllHomecomponents from "./components/pages/AllHomecomponents.jsx";
+import ProductDetails from "./components/pages/ProductDetails.jsx";
+import Collections from "./components/pages/Collections.jsx";
+import Contact from "./components/pages/Contact.jsx";
+import About from "./components/pages/About.jsx";
+
 const App = () => {
   return (
     <div>
-
       <Routes>
         {/* Home Page */}
-      <Route path="/" element={<AllHomecomponents />} />
-      <Route path='/ProductDetails' element={<ProductDetails />} />
-
-
+        <Route path="/" element={<AllHomecomponents />} />
+        <Route path="/ProductDetails" element={<ProductDetails />} />
+        <Route path="/Collections" element={<Collections />} />
+        <Route path="/Contact " element={<Contact />} />
+        <Route path="/About" element={<About />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ Client/src/components/pages/Collections.jsx
@@ -0,0 +1,9 @@
+const Collections = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Collections

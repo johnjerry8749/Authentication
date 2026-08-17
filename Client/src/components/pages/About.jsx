a7efd424c1e96a9a8a2712a4647a748a57d4c876
John <johnjerry8749@gmail.com>
2026-08-18 00:24:02 +0100

    im-lement contact and about pages

--- Client/src/components/pages/About.jsx
+++ Client/src/components/pages/About.jsx
@@ -6,16 +6,24 @@ function About() {
   return (
     <div>
       <Navbar />
-      <div className="border border-danger mb-4 p-2">
+      <div className="mb-4 p-2">
         <div className="d-flex justify-content-center mt-4 align-items-center">
-        <h3 className="mt-3 text-center fw-bold text-muted">ABOUT <span className="text-dark">US</span></h3>
-        <div className="mt-3  " style={{width:"30px", height:"2px", backgroundColor:"black"}}></div>
+          <h3 className="mt-3 text-center fw-bold text-muted">
+            ABOUT <span className="text-dark">US</span>
+          </h3>
+          <div
+            className="mt-3  "
+            style={{ width: "30px", height: "2px", backgroundColor: "black" }}
+          ></div>
         </div>
-
+        <div className="row border border-danger m-auto p-2 d-flex justify-content-center align-items-center gap-2">
+          <div className="col-10 col-lg-5 border border-danger p-2"></div>
+          <div className="col-10 col-lg-5 border border-danger p-2"></div>
+        </div>
       </div>
-        
-    <Newsletter />    
-    <Footer />
+
+      <Newsletter />
+      <Footer />
     </div>
   );
 }
--- Client/src/components/pages/Contact.jsx
+++ Client/src/components/pages/Contact.jsx
@@ -1,22 +1,30 @@
-import Navbar from "../common/Navbar"
-import Footer from "../common/Footer"
-import Newsletter from "./Newsletter"
+import Navbar from "../common/Navbar";
+import Footer from "../common/Footer";
+import Newsletter from "./Newsletter";
 
 const Contact = () => {
   return (
     <div>
       <Navbar />
-      <div className="border border-danger mb-4 p-2">
+      <div className=" mb-4 p-2">
         <div className="d-flex justify-content-center mt-4 align-items-center">
-        <h3 className="mt-3 text-center fw-bold text-muted">CONTACT <span className="text-dark">US</span></h3>
-        <div className="mt-3  " style={{width:"30px", height:"2px", backgroundColor:"black"}}></div>
+          <h3 className="mt-3 text-center fw-bold text-muted">
+            CONTACT <span className="text-dark">US</span>
+          </h3>
+          <div
+            className="mt-3  "
+            style={{ width: "30px", height: "2px", backgroundColor: "black" }}
+          ></div>
         </div>
-
+        <div className="row border border-danger m-auto p-2 d-flex justify-content-center align-items-center gap-2">
+          <div className="col-10 col-lg-5 border border-danger p-2"></div>
+          <div className="col-10 col-lg-5 border border-danger p-2"></div>
+        </div>
       </div>
-      <Newsletter/>
+      <Newsletter />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;

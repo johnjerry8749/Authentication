72e2c381a5774c617b0d9d5a84e1daf55617f33c
John <johnjerry8749@gmail.com>
2026-08-18 00:14:32 +0100

    save

--- Client/src/components/pages/About.jsx
+++ Client/src/components/pages/About.jsx
@@ -1,13 +1,15 @@
 import Navbar from "../common/Navbar";
 import Footer from "../common/Footer";
+import Newsletter from "./Newsletter";
 
 function About() {
   return (
     <div>
       <Navbar />
       <div className="border border-danger p-2"></div>
-
-      <Footer />
+        
+    <Newsletter />    
+    <Footer />
     </div>
   );
 }
--- Client/src/components/pages/Contact.jsx
+++ Client/src/components/pages/Contact.jsx
@@ -1,12 +1,19 @@
 import Navbar from "../common/Navbar"
 import Footer from "../common/Footer"
+import Newsletter from "./Newsletter"
 
 const Contact = () => {
   return (
     <div>
       <Navbar />
-      <div className="border border-danger p-2"></div>
+      <div className="border border-danger mb-4 p-2">
+        <div>
+        <h3 className=" border border-danger mt-3 text-center fw-bold text-muted">ABOUT <span className="text-dark">US</span></h3>
+        <div style={{width:"50px", height:"1px", backgroundColor:"black"}}></div>
+        </div>
 
+      </div>
+      <Newsletter/>
       <Footer />
     </div>
   )

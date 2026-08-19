61e1d60a2383d53a82686d3874ac4611edbc017f
John <johnjerry8749@gmail.com>
2026-08-19 15:39:13 +0100

    save

--- Client/src/components/pages/About.jsx
+++ Client/src/components/pages/About.jsx
@@ -27,19 +27,27 @@ function About() {
             />
           </div>
           <div className="col-10 col-lg-5 border border-danger p-2">
-            <p>
+            <p className="fs-5 p-2">
               Forever was born out of a passion for innovation and a desire to
               revolutionize the way people shop online. Our journey began with a
               simple idea: to provide a platform where customers can easily
               discover, explore, and purchase a wide range of products from the
               comfort of their homes.
             </p>
-            <p>
+            <p className="fs-5 p-2">
               Since our inception, we've worked tirelessly to curate a diverse
               selection of high-quality products that cater to every taste and
               preference. From fashion and beauty to electronics and home
               essentials, we offer an extensive collection sourced from trusted
               brands and suppliers.
+            </p>
+
+            <h5>Our Mission</h5>
+            <p className="fs-5 p-2">
+              Our mission at Forever is to empower customers with choice,
+              convenience, and confidence. We're dedicated to providing a
+              seamless shopping experience that exceeds expectations, from
+              browsing and ordering to delivery and beyond.
             </p>
           </div>
         </div>

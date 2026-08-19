7260fedd71511ee3e29e7dd00088fb41a727f56b
John <johnjerry8749@gmail.com>
2026-08-19 16:12:13 +0100

    save

--- Client/src/components/pages/Contact.jsx
+++ Client/src/components/pages/Contact.jsx
@@ -1,7 +1,7 @@
 import Navbar from "../common/Navbar";
 import Footer from "../common/Footer";
 import Newsletter from "./Newsletter";
-import Contactimg from "../../assets/front/contact_img.png"
+import Contactimg from "../../assets/front/contact_img.png";
 
 const Contact = () => {
   return (
@@ -17,11 +17,25 @@ const Contact = () => {
             style={{ width: "30px", height: "2px", backgroundColor: "black" }}
           ></div>
         </div>
-        <div className="row border border-danger mt-4 m-auto p-2 d-flex justify-content-center align-items-center gap-2">
-          <div className="col-10 col-lg-5 border  border-danger p-2">
-            <img src={Contactimg} alt="" className="p-2" style={{width:"100%", height:"100%"}} />
+        <div className="row mt-4 m-auto p-2 d-flex justify-content-center align-items-center gap-2">
+          <div className="col-10 col-lg-5 p-2">
+            <img
+              src={Contactimg}
+              alt=""
+              className="p-4"
+              style={{ width: "100%", height: "100%" }}
+            />
           </div>
-          <div className="col-10 col-lg-5 border border-danger p-2"></div>
+          <div className="col-10 col-lg-5 p-2">
+            <h5 className="fs-3 fw-3">Our Store</h5>
+            <p className="fs-6 mb-0">54709 Willms Station</p>
+            <p className="fs-6 mb-3">Suite 350, Washington, USA</p>
+
+            <p className="fs-6 mb-0">Tel: (415) 555-0132</p>
+            <p className="fs-6">Email: admin@forever.com</p>
+
+            
+          </div>
         </div>
       </div>
       <Newsletter />

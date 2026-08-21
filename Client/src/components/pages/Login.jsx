1e894a525bf3affb04f6bdbcb640aa52b99e3e6e
John <johnjerry8749@gmail.com>
2026-08-21 17:13:17 +0100

    save

--- Client/src/components/pages/Login.jsx
+++ Client/src/components/pages/Login.jsx
@@ -1,8 +1,17 @@
+import Footer from "../common/Footer"
+import Navbar from "../common/Navbar"
 
 const Login = () => {
   return (
     <div>
-      
+      <Navbar />
+      <div className="border border-danger d-flex justify-content-center align-items-center p-2">
+
+
+      </div>
+
+
+      <Footer />
     </div>
   )
 }
